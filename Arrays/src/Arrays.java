9b4c7e35eddb7e7bf4079920bba13fb3d08e3457
thorben <thorben@thorben-ThinkPad-T480s>
2024-10-16 22:56:56 +0200

    return 1

--- Arrays/src/Arrays.java
+++ Arrays/src/Arrays.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Arrays {
-
+//test vom laptop
 	public static void main(String[] args) {
 		// TODO A uto-generated method stub
-		return;
+		return 1; 
 	}
 
 }
